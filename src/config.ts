4f50de8d1795b9e4a5490ed1e0261cd9d8392931
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-04-30 15:29:59 +0500

    gis ext widget

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import type { ImmutableObject } from 'seamless-immutable'
+
+export interface Config {
+  showScale: boolean
+  showZoom: boolean
+}
+
+export type IMConfig = ImmutableObject<Config>
